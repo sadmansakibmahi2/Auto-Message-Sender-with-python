3c309b12290beb3b9f33bf63dcbe2b3a1e4cd551
Sadman Sakib Mahi <86464097+sadmansakibmahi2@users.noreply.github.com>
2022-01-31 13:09:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import time
+import random
 import pyautogui # go to this link to install this library https://pypi.org/project/PyAutoGUI/
-msg = 'hello'# your msg
+msg_list =["How are you?","how old are you?","What is your age?","Where are you from"]          # your msg
 number_of_msg= 35 # number of msg you want to send
 count = 0
 time.sleep(4)
 while number_of_msg>0 :
 	number_of_msg = number_of_msg - 1
 	count = count + 1
+	msg= random.choice(msg_list)
 	print(count)
 	print(msg)
 	pyautogui.write(msg)
 	pyautogui.press('enter')
-	time.sleep(2)
+	time.sleep(1)
